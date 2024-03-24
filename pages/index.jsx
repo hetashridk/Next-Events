ad4d43845f155bd6c51784827c3c9efb742ae24d
hetashridk <hetashrik@gmail.com>
2024-03-24 22:57:32 +0530

    event-item components  added in home page which main route

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,14 @@
+import EventList from '@/components/events/event-list';
+import { getFeaturedEvents } from '@/dummy-data'
 import React from 'react'
 
 function HomePage() {
+
+  const featuredEvents = getFeaturedEvents();
   return (
-    <div>HomePage</div>
+    <div>
+      <EventList />
+    </div>
   )
 }
 
