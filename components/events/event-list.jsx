027b9bb868d70c44922e2b73115eb404cd9fbfb4
hetashridk <hetashrik@gmail.com>
2024-03-24 22:56:16 +0530

    event-list added

---
+++ components/events/event-list.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EventItem from './event-item'
+
+function EventList({ items }) {
+  return (
+    <ul>
+        {items.map(event => <EventItem key={event.id} id={event.id} title={event.title} image={event.image} location={event.location} date={event.date}/>)}
+    </ul>
+  )
+}
+
+export default EventList
