ac49408bd42ba854adc8c83d3f4a2dfe8f2d5e4b
hetashridk <hetashrik@gmail.com>
2024-03-27 22:21:31 +0530

    css added

--- components/events/event-item.jsx
+++ components/events/event-item.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
-import Link from 'next/link'
+import Link from 'next/link';
+import classes from './event-item.module.css'
+
 function EventItem(props) {
-    const { id, title, date, image, location } = props
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    })
+  const { id, title, date, image, location } = props;
 
-    const formattedAddress = location.replace(',', '\n');
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
 
-    const exploreLink = `/events/${id}`;
+  const formattedAddress = location.replace(', ', '\n');
+
+  const exploreLink = `/events/${id}`;
+
+
   return (
-    <li>
-        <img src={"/" + image} alt={title}  />
-        <div>
-            <div>
+    <li className={classes.item}>
+        <img src={'/' + image} alt={title}  />
+        <div className={classes.content}>
+            <div className={classes.summary}>
                 <h2>{title}</h2>
-                <div>
+                <div className={classes.date}>
                     <time>{humanReadableDate}</time>
                 </div>
-                <div>
+                <div className={classes.address}>
                     <address>{formattedAddress}</address>
                 </div>
             </div>
-            <div>
+            <div className={classes.actions}>
                 <Link href={exploreLink}>Explore more Events</Link>
             </div>
         </div>
--- components/events/event-list.jsx
+++ components/events/event-list.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import EventItem from './event-item'
+import classes from './event-list.module.css'
 
 function EventList(props) {
 
   const { items } = props;
   return (
-    <ul>
-        {items.map((event) => (<EventItem key={event.id} id={event.id} title={event.title} image={event.image} location={event.location}/>))}
+    <ul className={classes.list}>
+        {items.map((event) => (
+        <EventItem 
+        key={event.id} 
+        id={event.id} 
+        title={event.title} 
+        image={event.image} 
+        location={event.location}
+        />
+        ))}
     </ul>
   )
 }
