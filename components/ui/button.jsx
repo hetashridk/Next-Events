116670eba2f590dbf44a1d4d11b22883f003f9c5
hetashridk <hetashrik@gmail.com>
2024-03-30 22:57:18 +0530

    ui componnets (button) added

---
+++ components/ui/button.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+import classes from './button.module.css'
+
+function Button(props) {
+  return (
+    <Link href={props.link} className={classes.btn}>{props.children}</Link>
+  )
+}
+
+export default Button
