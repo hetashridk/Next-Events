c5838246028bf0d584279a412b64cfa43c66d7b6
hetashridk <hetashrik@gmail.com>
2024-04-01 22:42:12 +0530

    event dyamically added using [eventid]

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import EventContent from '@/components/event-detail/event-content';
+import EventLogistics from '@/components/event-detail/event-logistics';
+import EventSummary from '@/components/event-detail/event-summary';
+import { getEventById } from '@/dummy-data';
+import { useRouter } from 'next/router'
+import React, { Fragment } from 'react'
 
 function EventDetailPage() {
+
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if(!event){
+    return (
+    <p>No event found</p>
+    )
+  }
+
+
   return (
-    <div>EventDetailPage</div>
+    <Fragment>
+      <EventSummary title={event.title}/>
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        {event.description}
+      </EventContent>
+    </Fragment>
   )
 }
 
