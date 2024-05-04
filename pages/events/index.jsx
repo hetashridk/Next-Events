ec2c08f2c9437d9b0bc162afce703fe9a3b07407
hetashridk <hetashrik@gmail.com>
2024-05-04 12:17:16 +0530

    searching/filtering events added on all events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useRouter } from 'next/router';
+
+import EventList from '@/components/events/event-list';
+import EventSearch from '@/components/events/event-search';
+import { getAllEvents } from '@/dummy-data'
 
 function AllEventsPage() {
+
+  const router = useRouter();
+  const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `events/${year}/${month}`
+    router.push(fullPath);
+  }
   return (
-    <div>AllEventsPage</div>
+    <Fragment>
+      <EventSearch  onSearch={findEventsHandler}/>
+      <EventList items={events} />
+    </Fragment>
   )
 }
 
